96126f642a8a5e6d8e89a9d1a4fc60626f2bd5fe
Eri Schon <erischon@gmail.com>
2020-11-23 10:06:36 +0100

    Update database.py

--- thesubstitute/database.py
+++ thesubstitute/database.py
@@ -4,6 +4,7 @@ from config import *
 from mysql.connector import errorcode
 from tables import Tables
 
+
 class Database:
     """ """
 
@@ -12,7 +13,7 @@ class Database:
         self.host = HOST
         self.user = USER
         self.password = PASSWORD
-        self.db_name = 'PureBeurre'
+        self.db_name = "PureBeurre"
         self.tables = Tables()
 
     def connection(self):
@@ -20,16 +21,17 @@ class Database:
 
         try:
             self.connection = mysql.connector.connect(
-            host = self.host,
-            user = self.user,
-            password = self.password,
-            database = self.db_name)
+                host=self.host,
+                user=self.user,
+                password=self.password,
+                database=self.db_name,
+            )
 
             self.mycursor = self.connection.cursor()
 
-            if (self.connection.is_connected()):
+            if self.connection.is_connected():
                 print(f"REUSSITE : Connection à la base {self.db_name} effectuée.")
-            
+
             return self.mycursor
 
         except mysql.connector.Error as error:
@@ -38,11 +40,13 @@ class Database:
     def db_create(self):
         """ """
         mycursor = self.connection()
-        
+
         try:
-            mycursor.execute("CREATE DATABASE {} DEFAULT CHARACTER SET 'utf8'".format(self.db_name))
+            mycursor.execute(
+                "CREATE DATABASE {} DEFAULT CHARACTER SET 'utf8'".format(self.db_name)
+            )
             print(f"REUSSITE : création de la base {self.db_name} effectuée.")
-        
+
         except mysql.connector.Error as err:
             print("ECHEC : impossible de créer la base, erreur : {}".format(err))
             exit(1)
@@ -53,11 +57,16 @@ class Database:
 
         for table_name in self.tables.TABLES:
             table_description = self.tables.TABLES[table_name]
-            
+
             try:
                 mycursor.execute(table_description)
-                print("REUSSITE : la création de la table {} est effectuée.\n".format(table_name), end='')
-            
+                print(
+                    "REUSSITE : la création de la table {} est effectuée.\n".format(
+                        table_name
+                    ),
+                    end="",
+                )
+
             except mysql.connector.Error as err:
                 print("ECHEC : impossible de créer la table, erreur : {}".format(error))
 
@@ -65,24 +74,25 @@ class Database:
         mycursor = self.connection()
 
         try:
-            add_nutriscore = ("INSERT INTO nutriscore (nut_id, nut_type) VALUES (%s,%s)")
-            values = (1, 'A')
+            add_nutriscore = "INSERT INTO nutriscore (nut_id, nut_type) VALUES (%s,%s)"
+            values = (1, "A")
             self.mycursor.execute(add_nutriscore, values)
-            values = (2, 'B')
-            self.mycursor.execute(add_nutriscore, values)      
-            values = (3, 'C')
-            self.mycursor.execute(add_nutriscore, values) 
-            values = (4, 'D')
-            self.mycursor.execute(add_nutriscore, values) 
-            values = (5, 'E')
-            self.mycursor.execute(add_nutriscore, values) 
-            
+            values = (2, "B")
+            self.mycursor.execute(add_nutriscore, values)
+            values = (3, "C")
+            self.mycursor.execute(add_nutriscore, values)
+            values = (4, "D")
+            self.mycursor.execute(add_nutriscore, values)
+            values = (5, "E")
+            self.mycursor.execute(add_nutriscore, values)
+
             self.connection.commit()
 
             print("Les différents Nutriscore ont été chargés dans la base.")
 
         except mysql.connector.Error as error:
             print("Erreur lors du chargement : {}".format(error))
+
 
 if __name__ == "__main__":
     database = Database()
