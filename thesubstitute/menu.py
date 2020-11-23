e8c2a8e5cedd6e6eafecbd574f6cdabc08d6a937
Eri Schon <erischon@gmail.com>
2020-11-23 12:25:49 +0100

    Update menu.py

--- thesubstitute/menu.py
+++ thesubstitute/menu.py
@@ -6,5 +6,14 @@ class Menu:
     def __init__(self):
         self.setup = Setup()
 
-    
 
+    def menu_cat(self):
+        """ """
+        
+        for n in range(len(self.setup.cat_choice())):
+            print(f"{n+1} - {self.setup.cat_choice()[n][1]}")
+
+if __name__ == "__main__":
+    menu = Menu()
+
+menu.menu_cat()
