8d050dd11aa61d2426e75ffb840524497786eab6
Eri Schon <erischon@gmail.com>
2020-11-24 15:28:14 +0100

    Update requests.py

--- thesubstitute/requests.py
+++ thesubstitute/requests.py
@@ -38,27 +38,29 @@ class Requests:
     def product_infos(self, prod_id):
         """ I return all the infos for a product. """
 
-        query = f"SELECT prod_id, prod_nom, prod_url, nut_id FROM produits WHERE prod_id ='{prod_id}';"
-        result = self.query(query)
-
+        # Select in produits table.
+        query_prod = f"SELECT prod_id, prod_nom, prod_url, nut_id FROM produits WHERE prod_id ='{prod_id}';"
+        result = self.query(query_prod)
         prod_id, nom, url, nutriscore = result[0]
 
+        # Select in shops table.
         query_shop = f"SELECT s.shop_nom FROM shops s INNER JOIN prodshop ps ON ps.prod_id = '{prod_id}' AND ps.shop_id = s.shop_id;"
-        result = self.query(query_shop)
-        
+        result = self.query(query_shop)       
         shop_name = [result[n][0] for n in range(len(result))]
 
+        # Select in marques table.
         query_marque = f"SELECT m.marq_nom FROM marques m INNER JOIN prodmarq pm ON pm.prod_id = '{prod_id}' AND pm.marq_id = m.marq_id;"
         result = self.query(query_marque)
-
         marque_name = [result[n][0] for n in range(len(result))]
 
+        # Select the nutriscore
         query_nutri = f"SELECT nut_type FROM nutriscore WHERE nut_id = '{nutriscore}';"
         result = self.query(query_nutri)
-
         nut_type = result[0][0]
 
-        return prod_id, nom, url, nut_type, shop_name, marque_name
+        prod_infos = {'prod_id':prod_id, 'prod_name': nom, 'prod_url': url, 'prod_nut':nut_type, 'prod_shop': shop_name, 'prod_marq': marque_name }
+
+        return prod_infos
 
     def substitutes(self):
         pass
@@ -75,7 +77,6 @@ class Requests:
 
     def to_list_of_integer(self, l_o_t):
         """ Transform a list of tuple (l_o_t) to a list of integer (l_o_i) """
-
         l_o_i = [[str(x) for x in tup] for tup in l_o_t]
         l_o_i = ["".join(i) for i in l_o_i]
         l_o_i = [int(i) for i in l_o_i]
@@ -83,8 +84,7 @@ class Requests:
         return l_o_i
     
     def to_list_of_tuple(self, list_of_tuple, query, n):
-        """ Create the list of tuple and add the ranking """
-      
+        """ Create the list of tuple and add the ranking """ 
         value = self.query(query)      
         value = value[0]
         rank = list(value)
@@ -101,11 +101,5 @@ if __name__ == "__main__":
     # print(requests.cat_popular())
     print(requests.product_infos('3268840001008'))
 
-    # query = f"SELECT p.prod_id FROM produits p INNER JOIN prodcat pc ON pc.cat_id = '11' AND pc.prod_id = p.prod_id;"
-
     # query = f"SELECT p.prod_id, p.prod_nom, p.prod_url, s.shop_nom, m.marq_nom FROM produits p, shops s, marques m INNER JOIN prodcat pc, prodshop ps, prodmarq pm ON pc.cat_id = '11' AND pc.prod_id = p.prod_id AND ps.prod_id = p.prod_id AND pm.prod_id = p.prod_id;"
-    # query_product = f"SELECT p.prod_id, p.prod_nom, p.prod_url, p.nut_id FROM produits p INNER JOIN prodcat pc ON pc.cat_id = '11' AND pc.prod_id = p.prod_id;"
-    # query_shop = f"SELECT s.shop_nom FROM shops s INNER JOIN prodshop ps ON ps.prod_id = '3274080005003' AND ps.shop_id = s.shop_id;"
-    # query_marque = f"SELECT m.marq_nom FROM marques m INNER JOIN prodmarq pm ON pm.prod_id = '3274080005003' AND pm.marq_id = m.marq_id;"
-    # query_nutri = f"SELECT nut_type FROM nutriscore WHERE nut_id = '1';"
     
