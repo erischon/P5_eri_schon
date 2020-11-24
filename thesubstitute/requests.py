b0c110ff1898f6d8c5b91470aa18e3a436c7725b
Eri Schon <erischon@gmail.com>
2020-11-24 10:30:01 +0100

    Update requests.py

--- thesubstitute/requests.py
+++ thesubstitute/requests.py
@@ -10,29 +10,32 @@ class Requests:
         self.mycursor = self.database.connection()
 
     def cat_popular(self):
-        """ """
-        # requete dans la base pour savoir quels sont les catégories les plus utilisées
-        # retourner une liste
-
+        """ I return the list of the 10 most popular categories """
+        # Search the most popular categories (categories with the most products)
         query = f"SELECT cat_id FROM prodcat GROUP BY cat_id ORDER BY COUNT(*) DESC LIMIT 10;"
 
         self.mycursor.execute(query)
         result = self.mycursor.fetchall()
 
+        # Transform a list of tuple to a list of integer
         result = [[str(x) for x in tup] for tup in result]
-        result = [''.join(i) for i in result]
+        result = ["".join(i) for i in result]
         result = [int(i) for i in result]
 
         cat_popular = []
 
+        # Create the list of tuple and add the ranking
         for n in range(len(result)):
-            query = f"SELECT cat_id, cat_nom FROM categories WHERE cat_id = '{result[n]}'"
+            query = (
+                f"SELECT cat_id, cat_nom FROM categories WHERE cat_id = '{result[n]}'"
+            )
             self.mycursor.execute(query)
             categorie = self.mycursor.fetchall()
             categorie = categorie[0]
-            x = list(categorie)
-            x.append(n)
-            categorie = tuple(x)
+            # add ranking
+            rank = list(categorie)
+            rank.append(n)
+            categorie = tuple(rank)
             cat_popular.append(categorie)
 
         return cat_popular
@@ -41,4 +44,4 @@ class Requests:
 if __name__ == "__main__":
     requests = Requests()
 
-print(requests.cat_popular())
+# print(requests.cat_popular())
