82c93d6d9d432e1f748132d916e43eaa0932ec4d
Eri Schon <erischon@gmail.com>
2020-11-24 12:28:13 +0100

    Update requests.py

--- thesubstitute/requests.py
+++ thesubstitute/requests.py
@@ -11,56 +11,43 @@ class Requests:
 
     def cat_popular(self):
         """ I return the list of the 10 most popular categories """
-        # Search the most popular categories (categories with the most products)
+        list_of_tuple = []
+
         query = f"SELECT cat_id FROM prodcat GROUP BY cat_id ORDER BY COUNT(*) DESC LIMIT 10;"
+
+        self.mycursor.execute(query)
+        result = self.to_list_of_integer(self.mycursor.fetchall())   
+
+        for n in range(len(result)):
+            query = (f"SELECT cat_id, cat_nom FROM categories WHERE cat_id = '{result[n]}'")
+            cat_popular = self.to_list_of_tuple(list_of_tuple, query, n)
+
+        return cat_popular
+
+    def product_list(self, cat_id):
+        """ """
+        list_of_tuple = []
+        # query = f"SELECT p.prod_id, p.prod_nom FROM produits p INNER JOIN prodcat pc ON pc.prod_id = p.prod_id;"
+        query = f"SELECT prod_id FROM prodcat WHERE cat_id = '{cat_id}';"
 
         self.mycursor.execute(query)
         result = self.to_list_of_integer(self.mycursor.fetchall())
 
-        cat_popular = []
-
-        # Create the list of tuple and add the ranking
         for n in range(len(result)):
-            query = (
-                f"SELECT cat_id, cat_nom FROM categories WHERE cat_id = '{result[n]}'"
-            )
-            self.mycursor.execute(query)
-            categorie = self.mycursor.fetchall()
-            categorie = categorie[0]
-            # add ranking
-            rank = list(categorie)
-            rank.append(n)
-            categorie = tuple(rank)
-            cat_popular.append(categorie)
+            query = (f"SELECT prod_id, prod_nom FROM produits WHERE prod_id = '{result[n]}'")
+            prod_list = self.to_list_of_tuple(list_of_tuple, query, n)
 
-        return cat_popular
+        return prod_list
 
-    def product_list(self, cat):
+    def product_infos(self, prod_id):
         """ """
-        # query = f"SELECT p.prod_id, p.prod_nom FROM produits p INNER JOIN prodcat pc ON pc.prod_id = p.prod_id;"
-        query = f"SELECT prod_id FROM prodcat WHERE cat_id = '{cat}';"
+        query = f"SELECT prod_nom, prod_url FROM produits WHERE prod_id = '{prod_id}';"
 
         self.mycursor.execute(query)
         result = self.mycursor.fetchall()
 
-        result = [[str(x) for x in tup] for tup in result]
-        result = ["".join(i) for i in result]
-        result = [int(i) for i in result]
+        print(result) 
 
-        prod_list = []
-
-        for n in range(len(result)):
-            query = (f"SELECT prod_id, prod_nom FROM produits WHERE prod_id = '{result[n]}'")
-            self.mycursor.execute(query)
-            product = self.mycursor.fetchall()
-            product = product[0]
-            # add a ranking
-            rank = list(product)
-            rank.append(n)
-            product = tuple(rank)
-            prod_list.append(product)
-
-        return prod_list
 
     def to_list_of_integer(self, l_o_t):
         """ Transform a list of tuple (l_o_t) to a list of integer (l_o_i) """
@@ -70,10 +57,23 @@ class Requests:
         l_o_i = [int(i) for i in l_o_i]
 
         return l_o_i
+    
+    def to_list_of_tuple(self, list_of_tuple, query, n):
+        """ Create the list of tuple and add the ranking """
+      
+        self.mycursor.execute(query)
+        value = self.mycursor.fetchall()      
+        value = value[0]
+        rank = list(value)
+        rank.append(n)
+        value = tuple(rank)
+        list_of_tuple.append(value)
 
+        return list_of_tuple
 
 if __name__ == "__main__":
     requests = Requests()
 
     # print(requests.product_list(11))
-    print(requests.cat_popular())
+    # print(requests.cat_popular())
+    print(requests.product_infos('3268840001008'))
