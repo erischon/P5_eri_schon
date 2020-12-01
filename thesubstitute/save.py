afe07c898bc514be806205230c4a0c4932a190d2
Eri Schon <erischon@gmail.com>
2020-12-01 10:17:59 +0100

    Update save.py

--- thesubstitute/save.py
+++ thesubstitute/save.py
@@ -8,18 +8,26 @@ class Save:
         self.connection = Connection()
 
     def saving(self, prod_id):
-        """ """
-        try:
-            query = f"INSERT INTO sauvegardes SET save_time='{datetime.datetime.now()}', prod_id='{prod_id}'"
-            print(query)
-            self.connection.execute(query)
-            self.connection.commit()
-        except:
-            print("La sauvegarde n'a pas pu se faire.") 
+        """ I save the substitut into the sauvegardes table. """
+        check = self.check(prod_id)
+
+        if check == False:
+            try:
+                query = f"INSERT INTO sauvegardes SET save_time='{datetime.datetime.now()}', prod_id='{prod_id}'"
+                self.connection.execute(query)
+                self.connection.commit()
+            except:
+                print("La sauvegarde n'a pas pu se faire.")
+        else:
+            print(check) 
 
     def sav_listing(self):
         """ """
-        pass
+        query = f"SELECT * FROM sauvegardes"
+        self.connection.execute(query)
+        result = self.connection.fetchall()
+
+        print(result)
 
     def sav_display(self):
         """ """
@@ -29,8 +37,21 @@ class Save:
         """ """
         pass
 
+    def check(self, prod_id):
+        """ I Check if a value is in a table, if yes I return its id """
+        query = f"SELECT save_id FROM sauvegardes WHERE prod_id LIKE '{prod_id}'"
+        self.connection.execute(query)
+        result = self.connection.fetchall()
+
+        if len(result) == 0:
+            return False
+        else:
+            return "Vous avez déjà sauvegardé ce substitut."
+
 if __name__ == "__main__":
     save = Save()
 
     ### Tests of methods ###
-    save.saving('3229820019307')
+    # save.saving('3229820019307')
+    # print(save.check('3229820019307'))
+    save.sav_listing()
