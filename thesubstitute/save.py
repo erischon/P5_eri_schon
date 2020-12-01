72de0ca08070943909169f1fd8108a8388eb975d
Eri Schon <erischon@gmail.com>
2020-12-01 09:20:00 +0100

    Create save.py

---
+++ thesubstitute/save.py
@@ -0,0 +1,29 @@
+from connection import Connection
+
+class Save:
+    """ """
+
+    def __init__(self):
+        pass
+
+    def saving(self, prod_id):
+        """ """
+        pass
+
+    def sav_listing(self):
+        """ """
+        pass
+
+    def sav_display(self):
+        """ """
+        pass
+
+    def sav_delete(self, save_id):
+        """ """
+        pass
+
+if __name__ == "__main__":
+    save = Save()
+
+    ### Tests of methods ###
+    save.saving('3229820019307')
