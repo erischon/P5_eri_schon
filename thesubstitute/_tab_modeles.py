b6c32919049d199bd96c2649d83cecfa2e27e6d3
Eri Schon <erischon@gmail.com>
2020-12-03 11:46:26 +0100

    Create _tab_modeles.py

---
+++ thesubstitute/_tab_modeles.py
@@ -0,0 +1,34 @@
+class ModCategories:
+    """ I'm categories. """
+
+    def __init__(self):
+        self.id_target = "cat_id"
+        self.table_target = "categories"
+        self.column_target = "cat_nom"
+
+
+class ModMarques:
+    """ I'm marques. """
+
+    def __init__(self):
+        self.id_target = "marq_id"
+        self.table_target = "marques"
+        self.column_target = "marq_nom"
+
+
+class ModShops:
+    """ I'm shops. """
+
+    def __init__(self):
+        self.id_target = "shop_id"
+        self.table_target = "shops"
+        self.column_target = "shop_nom"
+
+
+class ModProduits:
+    """ I'm produits. """
+
+    def __init__(self):
+        self.id_target = "prod_id"
+        self.table_target = "produits"
+        self.column_target = "prod_id"
